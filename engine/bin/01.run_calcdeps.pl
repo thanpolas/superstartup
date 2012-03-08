ea45a919648e3026816bf5424944c9d2190e3621
Thanasis Polychronakis <thanpolas@gmail.com>
2012-03-08 16:45:55 +0200

    Working on deployment automation and removed excess code in main.ui.js

--- engine/bin/01.run_calcdeps.pl
+++ engine/bin/01.run_calcdeps.pl
@@ -8,8 +8,7 @@ use Time::Local;
 ### date stamp
 $dateStamp = strftime("%d-%m-%Y", localtime);
 ### Project Root
-#$projectRoot = "/home/projects/geowarp/03.devel";
-$projectRoot = "../../";
+$projectRoot = "../..";
 $jsroot = $projectRoot . "/html/js";
 $closurelib = $jsroot . "/closure-library";
 $googPath = $jsroot . "/closure-library/closure/goog";
@@ -20,9 +19,6 @@ $geoPath = $jsroot . "/core";
 
 $calcdeps = $closurelib . "/closure/bin/calcdeps.py";
 
-#$closurebuilder = $closurelib . "/closure/bin/build/closurebuilder.py";
-#$closurecompiler = $projectRoot . "/engine/bin/closure_compiler/compiler.jar";
-
 $java = "/usr/bin/java";
 
 
@@ -31,13 +27,6 @@ $java = "/usr/bin/java";
 
 
 $cmdrun = "$calcdeps -d $jsroot" . "/closure-library -p $sptPath -p $geoPath -o deps --output_file=$jsroot" . "/deps.js";
-
-#$cmdrun = "$calcdeps -i $geoPath" . "/main.js $sptPath" . "/main.js -p $jsroot" . "/closure-library $geoPath $sptPath  -o deps --output_file=$jsroot" . "/deps.js";
-
-#$cmdrun = "$calcdeps -i $geoPath" . "/main.js --path=$geoPath --path=$jsroot" . "/closure-library -o deps ";
-
-
-#$cmdrun = "$calcdeps  --path=$geoPath -o deps ";
 
 print "Will execute: " . $cmdrun . "\n";
 
