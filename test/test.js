3c73a0bbe4b7d75b411885d10c9a394413351954
Thanasis Polychronakis <thanpolas@gmail.com>
2012-12-20 01:32:01 +0200

    mocha-phantomjs now works

--- test/test.js
+++ test/test.js
@@ -1,10 +1,15 @@
-var assert = require("assert");
+
+var expect;
+
+expect = (typeof chai !== "undefined" && chai !== null ? chai.expect : void 0) || require('chai').expect;
 
 describe('Array', function(){
   describe('#indexOf()', function(){
     it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
+      expect([1,2,3].indexOf(5)).to.equal(-1);
+      expect([1,2,3].indexOf(0)).to.equal(-1);
+      //assert.equal(-1, [1,2,3].indexOf(5));
+      //assert.equal(-1, [1,2,3].indexOf(0));
     });
   });
 });
