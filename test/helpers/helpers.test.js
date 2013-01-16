f470cdfa360fee2e01697b3e04fd56330879e318
Thanasis Polychronakis <thanpolas@gmail.com>
2013-01-16 18:43:25 +0200

    changed helpers test to be unit

--- test/helpers/helpers.test.js
+++ test/helpers/helpers.test.js
@@ -1,69 +1,51 @@
 
 goog.provide('ssd.test.helpers');
 
-describe('Helpers', function() {
 
-  describe('ssd.arFind()', function() {
-    var arOne = [{id:1}, {id:2}, {id:3}, {id:4}, {id:5}];
-    var arTwo = [{fruit:'apples'}, {fruit:'oranges'}, {fruit:'grapes'}, {fruit:'bananas'}];
-    it('should work with numeric search value', function(){
-      expect(  ssd.arFind(arOne, 'id', 3).id  ).to.equal(3);
-    });
-    it('should work with string search value', function(){
-      expect(  ssd.arFind(arTwo, 'fruit', 'bananas').fruit  ).to.equal('bananas');
-    });
-    it('should return null when search yields no results', function(){
-      expect(  ssd.arFind(arOne, 'id', 9)  ).to.be.null;
-    });
-    it('should return null when a bogus key is provided', function(){
-      expect(  ssd.arFind(arOne, 'idid', 9)  ).to.be.null;
-    });
+suite('Helpers');
 
-    it('should not return references', function(){
-      var obj = ssd.arFind(arTwo, 'fruit', 'oranges');
-      obj.fruit = 'mango';
-      expect(  arTwo[2].fruit  ).to.not.equal(obj.fruit);
-    });
-  });
+test('isjQ', function(){
+  var jq = $('<div></div>');
+  var notjQ = new Array('2');
 
-  describe('ssd.arFindIndex()', function() {
-    var arOne = [{id:1}, {id:2}, {id:3}, {id:4}, {id:5}];
-    var arTwo = [{fruit:'apples'}, {fruit:'oranges'}, {fruit:'grapes'}, {fruit:'bananas'}];
-
-    it('should work with numeric search value', function(){
-      expect(  ssd.arFindIndex(arOne, 'id', 3)  ).to.equal(2);
-    });
-    it('should work with string search value', function(){
-      expect(  ssd.arFindIndex(arTwo, 'fruit', 'bananas')  ).to.equal(3);
-    });
-    it('should return -1 when search yields no result', function(){
-      expect(  ssd.arFindIndex(arOne, 'id', 9) ).to.equal(-1);
-    });
-    it('should return -1 when a bogus key is provided', function(){
-      expect(  ssd.arFindIndex(arOne, 'idid', 9)  ).to.equal(-1);
-    });
-  });
+  ok(ssd.isjQ(jq), 'A jQuery array');
+  ok(!ssd.isjQ(notjQ), 'Not a jQuery array');
+});
 
 
-  describe('ssd.arRemove()', function(){
-    var arOne = [{id:1}, {id:2}, {id:3}, {id:4}, {id:5}];
-    var arTwo = [{fruit:'apples'}, {fruit:'oranges'}, {fruit:'grapes'}, {fruit:'bananas'}];
+test('ssd.arFind', function() {
+  var arOne = [{id:1}, {id:2}, {id:3}, {id:4}, {id:5}];
+  var arTwo = [{fruit:'apples'}, {fruit:'oranges'}, {fruit:'grapes'}, {fruit:'bananas'}];
 
-    it('should return true when item removed', function(){
-      expect(  ssd.arRemove(arOne, 'id', 3)  ).to.be.True;
-    });
-    it('should return false when source is string instead of an array', function(){
-      expect(  ssd.arRemove('not an array', 'id', 3)  ).to.not.be.True;
-    });
-    it('should return false when wrong key is provided', function(){
-      expect(  ssd.arRemove(arOne, 'wrongKey', 3)  ).to.not.be.True;
-    });
-    it('should return false when passing an array to the key param', function(){
-      expect(  ssd.arRemove(arOne, [1, 2, 3], 3)  ).to.not.be.True;
-    });
-    it('resulting object should have a length of 4', function(){
-      expect(  arOne.length  ).to.equal(4);
-    });
-  });
+  equal(ssd.arFind(arOne, 'id', 3).id, 3, 'Searching with numeric value');
+  equal(ssd.arFind(arTwo, 'fruit', 'bananas').fruit, 'bananas', 'Searching with string value');
+  equal(ssd.arFind(arOne, 'id', 9), null, 'Search yields no result, we expect null');
+  equal(ssd.arFind(arOne, 'idid', 9), null, 'Bogus key, we expect null');
 
+  var obj = ssd.arFind(arTwo, 'fruit', 'oranges');
+  obj.fruit = 'mango';
+  notEqual(arTwo[2].fruit, obj.fruit, 'Returned objects are not references');
+});
+
+test('ssd.arFindIndex', function() {
+  var arOne = [{id:1}, {id:2}, {id:3}, {id:4}, {id:5}];
+  var arTwo = [{fruit:'apples'}, {fruit:'oranges'}, {fruit:'grapes'}, {fruit:'bananas'}];
+
+  equal(ssd.arFindIndex(arOne, 'id', 3), 2, 'Searching with numeric value');
+  equal(ssd.arFindIndex(arTwo, 'fruit', 'bananas'), 3, 'Searching with string value');
+  equal(ssd.arFindIndex(arOne, 'id', 9), -1, 'Search yields no result, we expect -1');
+  equal(ssd.arFindIndex(arOne, 'idid', 9), -1, 'Bogus key, we expect -1');
+});
+
+
+test('ssd.arRemove', function(){
+  var arOne = [{id:1}, {id:2}, {id:3}, {id:4}, {id:5}];
+  var arTwo = [{fruit:'apples'}, {fruit:'oranges'}, {fruit:'grapes'}, {fruit:'bananas'}];
+
+  ok(ssd.arRemove(arOne, 'id', 3), 'arRemove run normaly returns true');
+  ok(!ssd.arRemove('not an array', 'id', 3), 'arRemove with a string instead of array returns false');
+  ok(!ssd.arRemove(arOne, 'wrongKey', 3), 'Using a wrong key returns false');
+  ok(!ssd.arRemove(arOne, [1, 2, 3], 3), 'Using an array as a key returns false');
+
+  equal(arOne.length, 4, 'Our array\'s length now should be 4');
 });
