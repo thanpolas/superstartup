b45772480fd61b2c309942180b8ff2dd64054239
Thanasis Polychronakis <thanpolas@gmail.com>
2013-03-15 00:43:33 +0200

    more scafold

--- test/bdd/modules/userAuth/plugins/udo.plugin.auth.test.js
+++ test/bdd/modules/userAuth/plugins/udo.plugin.auth.test.js
@@ -1,2 +1,21 @@
 goog.provide('ssd.test.userAuth.udoPlugin');
 
+
+/**
+ * [udoPlugin description]
+ * @return {[type]} [description]
+ */
+ssd.test.userAuth.udoPlugin = function() {
+
+};
+
+
+/**
+ * [run description]
+ * @return {[type]} [description]
+ */
+ssd.test.userAuth.udoPlugin.prototype.run = function() {
+  describe('Perform auth / deauth method tests for plugin:', function(){
+
+  });
+};
